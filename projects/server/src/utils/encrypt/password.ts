53881cff4f1909a1321444739385c5739f55faac
zhangyuzhu <1580006194@qq.com>
2025-07-19 12:42:02 +0800

    chore(server):更换bcrypt库

--- projects/server/src/utils/encrypt/password.ts
+++ projects/server/src/utils/encrypt/password.ts
@@ -1,16 +1,16 @@
-import * as bcrypt from 'bcrypt'
+import * as bcryptjs from 'bcryptjs'
 
 /**
  * 对密码进行加密
  */
 export async function encryptPassword(password: string) {
   const saltRounds = 10
-  return bcrypt.hash(password, saltRounds)
+  return bcryptjs.hash(password, saltRounds)
 }
 
 /**
  * 校验密码是否正确
  */
 export async function comparePassword(password?: string, hash?: string) {
-  return bcrypt.compare(password ?? '', hash ?? '')
+  return bcryptjs.compare(password ?? '', hash ?? '')
 }
